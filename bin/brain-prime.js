6ef34206d953d16d6473883dbd2bc09eb44bc4db
Ivan Litskevich <sxkote@gmail.com>
2023-02-10 13:21:38 +0300

    step-9: brain-prime done

---
+++ bin/brain-prime.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import greeting from '../src/cli.js';
+import {
+    attempts,
+    gameComplete,
+    gameQuestion,
+    generateRandomNumber,
+} from '../src/index.js';
+
+// START THE GAME
+const username = greeting();
+
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+let isCompleted = true;
+const isPrime = (number) => {
+    for (let i = 2; i < number; i += 1) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+for (let i = 0; i < attempts; i += 1) {
+    const number = generateRandomNumber(2, 20);
+    const correct = isPrime(number) ? 'yes' : 'no';
+    isCompleted = gameQuestion(number, correct);
+    if (!isCompleted) {
+        break;
+    }
+}
+
+gameComplete(isCompleted, username);
